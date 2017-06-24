a79feb0241b57a28a617ea861a5aa8212d0dbd31
laxmanverma <laxmanbravolook@gmail.com>
2017-06-24 17:28:47 +0530

    ApplicationContext with property initialization

--- Spring-Framework/SpringDemo/src/org/laxman/spring/Triangle.java
+++ Spring-Framework/SpringDemo/src/org/laxman/spring/Triangle.java
@@ -1,7 +1,18 @@
 package org.laxman.spring;
 
 public class Triangle {
+	
+	private String type;
+	
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
 	public void draw(){
-		System.out.println("Triangle drawn");
+		System.out.println(getType() + " Triangle drawn");
 	}
 }
