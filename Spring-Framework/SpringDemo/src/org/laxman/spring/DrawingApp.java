21acc593cb39a525829f0f52dfdc3cf414c6033c
laxmanverma <laxmanbravolook@gmail.com>
2017-06-24 17:11:44 +0530

    ApplicationContext

--- Spring-Framework/SpringDemo/src/org/laxman/spring/DrawingApp.java
+++ Spring-Framework/SpringDemo/src/org/laxman/spring/DrawingApp.java
@@ -2,15 +2,18 @@ package org.laxman.spring;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.FileSystemResource;
 
 public class DrawingApp {
 
 	public static void main(String[] args) {
 		//Triangle triangle = new Triangle();
-		
-		BeanFactory factory = new XmlBeanFactory(new FileSystemResource("Spring.xml"));
-		Triangle triangle = (Triangle) factory.getBean("triangle");
+		//BeanFactory factory = new XmlBeanFactory(new FileSystemResource("Spring.xml"));
+		//Triangle triangle = (Triangle) factory.getBean("triangle");
+		ApplicationContext context = new ClassPathXmlApplicationContext("Spring.xml");
+		Triangle triangle = (Triangle) context.getBean("triangle");
 		triangle.draw();
 	}
 
